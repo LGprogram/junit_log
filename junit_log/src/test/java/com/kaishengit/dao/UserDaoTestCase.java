29e5a907f3838959b354f75f729e8f560c1e4471
LGzz <15037317516@139.com>
2016-12-12 23:12:25 +0800

    提交junit_log

---
+++ junit_log/src/test/java/com/kaishengit/dao/UserDaoTestCase.java
@@ -0,0 +1,49 @@
+package com.kaishengit.dao;
+
+import com.kaishengit.dao.UserDao;
+import com.kaishengit.entity.User;
+import static org.junit.Assert.*; //静态导入
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+public class UserDaoTestCase {
+
+    private UserDao userDao;
+
+    @Before
+    public void before() {
+        //....
+        userDao = new UserDao();
+    }
+
+    @Test
+    public void testSave() {
+        User user = new User();
+        user.setUsername("junit");
+        user.setAge(4);
+        user.setTel("028-2394825");
+        user.setAddress("美国");
+
+        userDao.save(user);
+    }
+
+    @Test
+    public void testFindById() {
+        User user = userDao.findById(1);
+
+        assertNotNull(user);
+    }
+
+    @Test
+    public void testFindAll() {
+        List<User> userList = userDao.findAll();
+        assertEquals(1,userList.size());
+    }
+
+    @Test
+    public void testDel() {
+        userDao.del(1);
+    }
+}
